a30de7ef2f53c284c8909d587dd52cd11787578c
idcetim <javirofe96@gmail.com>
2022-07-27 21:50:14 +0200

    Add routes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from './routes/Home'
+import Entradas from './routes/Entradas'
+import Produccion from './routes/Produccion'
+import VerStockEntradas from './routes/VerStockEntradas'
+import VerStockProduccion from './routes/VerStockProduccion'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return ( <div >
+    <BrowserRouter >
+    <Routes >
+      <Route path = "/" element = { < Home /> }/> 
+      <Route path = "/entradas" element = { < Entradas /> }/> 
+      <Route path = "/produccion"element = { < Produccion/> }/>
+      <Route path = "/verentradas" element = { < VerStockEntradas /> }/> 
+      <Route path = "/verproduccion" element = { < VerStockProduccion /> }/> 
+    </Routes> 
+    </BrowserRouter>
     </div>
-  );
+);
 }
 
 export default App;
