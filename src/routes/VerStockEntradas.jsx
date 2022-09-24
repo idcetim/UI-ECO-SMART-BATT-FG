5ba351d9b39a5b52be57fe9b4f03264a31edd418
idcetim <javirofe96@gmail.com>
2022-09-24 17:41:50 +0200

    Set fetch to obtain all the EntryLotesCodes

--- src/routes/VerStockEntradas.jsx
+++ src/routes/VerStockEntradas.jsx
@@ -1,3 +1,13 @@
+import { useState, useEffect} from 'react';
+import { entradas } from '../api/endpoints';
 export const VerStockEntradas = () => {
-    
+const [lotesCode, setLotesCode] = useState()
+
+  useEffect(()=> {
+    fetch(entradas).then((allLotes)=> {
+      setLotesCode(allLotes)
+    })
+  },[])
+    console.log(lotesCode)
+    return(<div></div>)
 }
