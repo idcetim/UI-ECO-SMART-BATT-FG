1837ca26a60afca0ed2cdb85b2de4416e741abf5
idcetim <javirofe96@gmail.com>
2022-10-08 12:15:36 +0200

    Add fantom link in the homePage

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -4,12 +4,17 @@ import '../styles/global.css'
 export const Home = () => {
     const smartContractAddress = '0xc7B23f2ffCfe6b466308dF175AA15c6202aa2a82'
     const smartContractUrl = `https://testnet.ftmscan.com/address/${smartContractAddress}`
+    const fantomUrl = "https://fantom.foundation/"
     return(
         <div className='web-wrapper'>
             <h1 className="main-h1"> Información blockchain </h1>
-            <a className='blockchain-link' href={smartContractUrl} target="_blank" rel="noreferrer"> Smart contract utilizado </a> 
-        
-    
+            <div className='url-home-link'>
+                <a className='blockchain-link' href={fantomUrl} target="_blank" rel="noreferrer"> Red de blockchain: Fantom </a>
+            </div>
+
+            <div className='url-home-link'>
+                <a className='blockchain-link' href={smartContractUrl} target="_blank" rel="noreferrer"> Smart contract  </a>
+            </div>
         </div>
        
     )
