a0b8e3e65f3519a917cadd0da3c74ed66ead8cd5
idcetim <javirofe96@gmail.com>
2022-11-14 22:29:31 +0100

    Update readme

--- src/routes/Stock.jsx
+++ src/routes/Stock.jsx
@@ -1,3 +1,4 @@
+ // Route para la parte de Gestion de Stock
 const Stock = () => {
   return(
     <div>
--- src/routes/Trazabilidad.jsx
+++ src/routes/Trazabilidad.jsx
@@ -9,26 +9,26 @@ const Trazabilidad = () =>  {
   const verStockEntradasHandler = () => {navigate('/trazabilidad/verentradas/')}
   const verStockProduccionHandler = () => {navigate('/trazabilidad/verproduccion/')}
   const verEstadisticasHandler = () => {navigate('/trazabilidad/estadisticas/')}
-  
+
   return(
       <div className='web-wrapper'>
           <h1 className="main-h1"> Trazabilidad </h1>
-      
+
           <button className="button-home" onClick={registrarHandler}>Registrar nueva entrada</button>
 
           <button className="button-home" onClick={entradasAnalisisHandler}>Añadir análisis de entrada</button>
-         
+
           <button className="button-home" onClick={produccionHandler}>Registrar nuevo producto</button>
 
           <button className="button-home" onClick={produccionAnalisisHandler}>Añadir análisis al producto</button>
-          
+
           <button className="button-home" onClick={verStockEntradasHandler}>Ver entradas</button>
-   
+
           <button className="button-home" onClick={verStockProduccionHandler}>Ver producción</button>
 
           <button className="button-home" onClick={verEstadisticasHandler}>Ver estadísticas</button>
       </div>
-     
+
   )
 
 }
