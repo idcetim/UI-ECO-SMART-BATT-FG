44d6a97f061c1bf4c0951a369e2c532b0d7a8e80
idcetim <javirofe96@gmail.com>
2023-01-03 09:08:58 +0100

    Cambiar url a azure

--- src/api/endpoints.js
+++ src/api/endpoints.js
@@ -1,8 +1,8 @@
 // export const url = "http://localhost:5000/"
 // export const url = "https://server-stock.azurewebsites.net/"
 
-export const url = "http://localhost:7071/api/"
-// export const url = "https://ferroglobe.azurewebsites.net/api/"
+//export const url = "http://localhost:7071/api/"
+export const url = "https://ferroglobe.azurewebsites.net/api/"
 
 export const registroEndpoints = {
     file: url + "file-example",
